fc7726dfdb7cdddaee1f2e82d3aa4ac5567af58b
Rohit-R-Khawale <rohitrkhawale@gmail.com>
2024-11-20 23:05:24 +0530

    Navbar demo removed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 function App() {
 return (
 <>
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti laborum culpa soluta totam odit blanditiis ipsum labore porro ducimus consequuntur.</h1>
 </>
 );}
 
