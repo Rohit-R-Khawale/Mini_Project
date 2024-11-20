352762e72e18a66eaa2de8d342ae45b2f922ef00
AtharvJoshi23 <joshiatharv2022@gmail.com>
2024-11-20 23:07:38 +0530

    changes to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 function Home() {
 return (
 <>
-
+<h1>Classroom Clone</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cumque iste odio accusamus impedit doloribus, beatae sed nam et quasi exercitationem atque eos ipsum maiores aliquam sapiente, magni fuga assumenda.</p>
 </>
 );}
 
